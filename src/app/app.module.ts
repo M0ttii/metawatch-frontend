adff94d09894ed156847567c3547c23a88b37b75
Adrian <adrian.doernberg@berlin.de>
2022-10-19 13:49:25 +0200

    Added chartjs
    Added chartjs,
    configured dashboard charts,
    fixed several things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ChartComponent } from './dashboard/chart/chart.component';
+import { NgChartsModule } from 'ng2-charts';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     NavbarComponent,
-    ChartComponent
+    ChartComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/chart/chart.component.ts
+++ src/app/dashboard/chart/chart.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
+import { ChartConfiguration, ChartOptions, } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, AfterViewInit{
 
-  constructor() { }
+  @ViewChild('myChart', {static: false}) myChart: ElementRef<HTMLCanvasElement>;
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+  private context: CanvasRenderingContext2D;
+  private endGradient: CanvasGradient = null;
+  colors = {
+    purple: {
+      default: "rgba(149, 76, 233, 1)",
+      half: "rgba(149, 76, 233, 0.5)",
+      quarter: "rgba(149, 76, 233, 0.25)",
+      zero: "rgba(149, 76, 233, 0)"
+    },
+    indigo: {
+      default: "rgba(80, 102, 120, 1)",
+      quarter: "rgba(80, 102, 120, 0.25)"
+    }
+  };
+
+  constructor() {
+  }
+
+  private getGradient(){ 
+    let gradient = this.context.createLinearGradient(0, 25, 0, 300);
+    gradient.addColorStop(0, this.colors.purple.half);
+    gradient.addColorStop(0.35, this.colors.purple.quarter);
+    gradient.addColorStop(1, this.colors.purple.zero);
+
+    return gradient;
+  }
+
+  public chartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'Test',
+      'Test1',
+      'Test2',
+      'Test3'
+    ],
+    datasets: [
+      {
+        data: [60, 58, 61.4, 62],
+        fill: true,
+        pointBackgroundColor: this.colors.purple.default,
+        borderColor: this.colors.purple.default,
+        tension: 0.2,
+        borderWidth: 2,
+        pointRadius: 3
+      }
+    ]
+  }
+
+  ngAfterViewInit(): void {
+    this.context = this.myChart.nativeElement.getContext('2d'); 
+    this.endGradient = this.getGradient();
+    this.chartData.datasets[0]['backgroundColor'] = this.endGradient;
+    this.chart.update();
+  }
 
   ngOnInit(): void {
+    
   }
+
+  public chartOptions: ChartOptions<'line'> = {
+    responsive: false,
+    layout: {
+      padding: 10
+    },
+    scales: {
+      x: {
+        ticks: {
+          padding: 10,
+          autoSkip: false,
+          maxRotation: 15,
+          minRotation: 15
+
+        },
+      },
+      y: {
+        ticks: {
+          maxTicksLimit: 63,
+          display: true,
+        },
+      },
+    },
+  };
 
 }
