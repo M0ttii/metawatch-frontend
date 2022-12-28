892cbcf25cdcd0199fe68b4870bfac1f5986a570
Adrian <adrian.doernberg@berlin.de>
2022-12-28 12:58:09 +0100

    .

--- src/app/dashboard/stats/stats.component.ts
+++ src/app/dashboard/stats/stats.component.ts
@@ -1,14 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Message } from 'src/app/models/message.model';
+import { SocketMessage } from 'src/app/models/socketmessage.model';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.css']
 })
-export class StatsComponent implements OnInit {
+export class StatsComponent implements OnInit, AfterViewInit{
   @Input() public type: string;
+  @Input() chartSubj: Subject<SocketMessage<Message>>;
+
+  protected cpu: string;
+  protected memory: string;
+  protected disk: string;
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+    switch(this.type){
+      case "cpu": {
+        this.chartSubj.subscribe((message) => {
+          this.cpu = message.message.cpu.perc.toString().substring(0, 3);
+        })
+        break;
+      }
+      case "memory": {
+        this.chartSubj.subscribe((message) => {
+          this.memory = message.message.memory.perc.toString().substring(0, 3);
+        })
+        break;
+      }
+      case "disk": {
+        this.chartSubj.subscribe((message) => {
+          this.disk = message.message.disk.read.toString().substring(0, 3);
+        })
+        break;
+      }
+    }
+  }
 
   ngOnInit(): void {
   }
